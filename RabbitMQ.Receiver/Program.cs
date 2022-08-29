62cc60ecd4f6db6b7e3b10e889a8b6d654f1e762
Денис Попович <popov.den30@gmail.com>
2022-08-29 11:58:53 +0300

    First commit

---
+++ RabbitMQ.Receiver/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var queueName = "A";
+var factory = new ConnectionFactory { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    //Durable говорит о том, что очередь будет оставаться после перезагрузки, если есть сообщения
+    channel.QueueDeclare(queueName, true, false, false);
+    
+    //Говорит о том, что обработка сообщений будет идти по мере завершения обработки, а не по очереди 
+    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.Span;
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine("Received {0}", message);
+        
+        Thread.Sleep(Random.Shared.Next(500, 1500));
+        
+        //Удаляем сообщение как только его обработали
+        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+    };
+
+    //Отключаем автоудаление (autoAck: false), чтобы избежать потери сообщения при отключении потребителя
+    channel.BasicConsume(queue: queueName,
+        autoAck: false,
+        consumer: consumer);
+    
+    Console.WriteLine("Press any button to exit...");
+    Console.Read();
+}
+
+
+
+
---
+++ RabbitMQ.Sender/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Text.Json.Serialization.Metadata;
+using RabbitMQ.Client;
+
+var queueName = "A";
+var exit = false;
+var factory = new ConnectionFactory { HostName = "localhost" };
+var count = 0;
+
+Console.CancelKeyPress += (sender, eventArgs) =>
+{
+    if (eventArgs.SpecialKey == ConsoleSpecialKey.ControlC)
+        exit = true;
+
+    eventArgs.Cancel = true;
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    //Durable говорит о том, что очередь будет оставаться после перезагрузки, если есть сообщения
+    var declarationResult = channel.QueueDeclare(queueName, true, false, false);
+    var basicProperties = channel.CreateBasicProperties();
+    basicProperties.Persistent = true; //Говорит о том, что сообщения будут сохранятся на диск
+
+    if (declarationResult == null)
+    {
+        Console.WriteLine("Cannot declare or connect to queue ");
+        return;
+    }
+
+    while (!exit)
+    {
+        Thread.Sleep(Random.Shared.Next(1000, 4000));
+        
+        try
+        {
+            var message = $"Message #{count++}";
+            var body = Encoding.UTF8.GetBytes(message);
+            
+            channel.BasicPublish(exchange: string.Empty,
+                                 routingKey: queueName,
+                                 basicProperties: basicProperties,
+                                 body: body);
+            
+            Console.WriteLine($"Message sent: {message}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
+
+Console.WriteLine("Press any button to exit...");
+Console.Read();
